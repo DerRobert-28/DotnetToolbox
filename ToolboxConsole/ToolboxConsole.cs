c6c2ecf45770dc07738883d1a2527dcd1474a800
Der Robert <erictainment@live.de>
2025-02-22 02:33:30 +0100

    rename

--- ToolboxConsole/ToolboxConsole.cs
+++ ToolboxConsole/ToolboxConsole.cs
@@ -1,9 +1,11 @@
 ﻿namespace DerRobert28.DotnetToolbox.ToolboxConsole;
 
-internal class ToolboxConsole
-{
-	public static void Main()
-	{
+internal class ToolboxConsole {
+
+	#region Main Method
+	public static void Main() {
 		Console.WriteLine("Hello, World!");
 	}
+
+	#endregion
 }
